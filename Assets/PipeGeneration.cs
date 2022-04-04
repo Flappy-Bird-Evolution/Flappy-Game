d305155a61f8171a74981dcc17fd558d08b7475b
Guy Rahamim <guy.danino5@gmail.com>
2022-04-04 11:36:40 +0300

    Added collider between pipes to aid in fitness calculations

--- Assets/PipeGeneration.cs
+++ Assets/PipeGeneration.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PipeGeneration : MonoBehaviour
 {
 	[SerializeField] GameObject Pipe;
+	[SerializeField] GameObject checkpoint;
 	[SerializeField] float startingX;
 	[SerializeField] int gapSize;
 
@@ -37,10 +38,13 @@ public class PipeGeneration : MonoBehaviour
 		GameObject upper = Instantiate(Pipe, new Vector2(startingX, upperHeight), Quaternion.identity);
 		upper.GetComponent<Pipe>().Init(Direction.DOWNWARDS, 20);
 
-		//int currentGapSize = (int) (gapSize * Random.Range(0.8f, 1f));
 		GameObject bottom = Instantiate(Pipe, new Vector2(startingX, upperHeight - gapSize), Quaternion.identity);
 		bottom.GetComponent<Pipe>().Init(Direction.UPWARDS, 20);
 
+		float checkpointY = (upper.transform.position.y + bottom.transform.position.y)/ 2f;
+		GameObject checkpointCollider = Instantiate(checkpoint, new Vector2(startingX, checkpointY), Quaternion.identity);
+		checkpointCollider.transform.SetParent(upper.transform);
+		
 		pairs.Add(new GameObject[] { upper, bottom });
 	}
 
